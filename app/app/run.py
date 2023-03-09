951f8534ad81e2edd80b1030262a7549cbbb5464
jmmsmulders <118716035+jmmsmulders@users.noreply.github.com>
2023-03-09 10:46:40 +0100

    Formatted HTML

--- app/app/run.py
+++ app/app/run.py
@@ -1,14 +1,8 @@
-import json
 import re
-import plotly
-import cv2
 import random
 import re
-import os
 from glob import glob
-import pandas as pd
 import numpy as np
-import requests
 from keras.layers import GlobalAveragePooling2D
 from keras.layers import Dense
 from keras.models import Sequential
@@ -48,15 +42,40 @@ train_files, train_targets = load_dataset('../../app/app/static/dogs')
 # load list of dog names
 dog_names = [item[20:-1] for item in sorted(glob("../../app/app/static/dogs/*/"))]
 
+
 def extract_Resnet50(tensor):
+    """
+    Function that extract features of the pre-trained ResNet50 model for classifying Dogs
+
+    Args:
+        tensor: numpy array of the image to predict for
+    
+    Output:
+        Prediction of the pre-trained model
+    """
     return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor), verbose=0)
 
-# Define a function to preprocess the image
-def preprocess_image(image):
-    image = image.img_to_array(img)
-    return np.expand_dims(image, axis=0)
+
+### returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img):
+    # Load model
+    ResNet50_model = ResNet50(weights='imagenet')
+    # define ResNet50 model
+    img = preprocess_input(img)
+    prediction = np.argmax(ResNet50_model.predict(img, verbose=0))
+    return ((prediction <= 268) & (prediction >= 151)) 
+
 
 def path_to_tensor(img_path):
+    '''
+    Function that reshapes an image to to tensors in the correct shape for the CNN-model
+
+    Args:
+        img_path: Location of the image
+    
+    Output:
+        Numpy array of the image in the shape (224, 224, 3)
+    '''
     # loads RGB image as PIL.Image.Image type
     img = image.load_img(img_path, target_size=(224, 224))
     # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
@@ -64,20 +83,6 @@ def path_to_tensor(img_path):
     # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
     return np.expand_dims(x, axis=0)
 
-def format_img(img_path):
-    '''
-    Function that takes an image paths, and formats it so all the displays are in a similar format.
-    
-    Args:
-        img_path: location of the image
-    
-    Output:
-        img: The formatted image
-    '''
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (1000, 1000))
-    return img
 
 def find_random_image_of_dog(idx):
     '''
@@ -110,7 +115,20 @@ def format_dog_name(idx):
     name = re.sub('[^0-9a-zA-Z]+', ' ', name)
     return name
 
+
 def find_top3(top_3):
+    """
+    Function that returns the top_3 most likely dog-classifcation for the selected image 
+    and also returns a random image of that breed
+
+    Args:
+        top_3: Dictionary, with the index of the breed on keys and the classification percentage as an item
+    
+    Output:
+        name_0, name_1, name_2: Name of the Dog breed with the top 3 most resemblance
+        percentage_0, percentage_1, percentage_2: Result of the algorithm corresponding to the percentage of classifcation
+        random_img_0, random_img_1, random_img_2: Random image of the predicted breed 
+    """
     top_3 = {k: v for k, v in sorted(top_3.items(), reverse=True, key=lambda item: item[1])}
 
     name_0 = format_dog_name(list(top_3.keys())[0])
@@ -153,12 +171,12 @@ def Resnet50_predict_breed(n=3):
     '''
     # Get the image file from the request
     image_file = request.files['image_file']
-
-    # Read the image file and preprocess it
-    # image = Image.open())
-    # image = preprocess_image(image)
-
     img = path_to_tensor(io.BytesIO(image_file.read()))
+
+    if dog_detector(img) == True:
+        s = 'Hello there doggie! \nLook at what a good boy you are!'            
+    else:
+        s = "This does not like a dog!\n Nevertheless, let's see what breed of dog this picture resembles most" 
 
     # extract bottleneck features
     bottleneck_feature = extract_Resnet50(img)
@@ -185,7 +203,8 @@ def Resnet50_predict_breed(n=3):
                     'percentage_2': percentage_2,
                     'random_img_0': random_img_0,
                     'random_img_1': random_img_1,
-                    'random_img_2': random_img_2})
+                    'random_img_2': random_img_2,
+                    'human_dog': s})
 
 def main():
     app.run(host='0.0.0.0', port=3000, debug=True)
